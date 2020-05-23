8f5c3e9f6e63ee93d9624e0e584d1bb10088f8d3
yct1995 <1160826661@qq.com>
2020-05-23 16:34:22 +0800

    更新1

--- mavendemo/src/main/java/com/example/mavendemo/TestGit.java
+++ mavendemo/src/main/java/com/example/mavendemo/TestGit.java
@@ -7,5 +7,6 @@ package com.example.mavendemo;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
+        System.out.println("更新1");
     }
 }
